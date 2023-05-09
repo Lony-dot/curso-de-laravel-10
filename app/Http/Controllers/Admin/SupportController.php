e8e9d7f51d75f28ac83d158130c87889e7fb656b
Lony <loonydarck@hotmail.com>
2023-05-09 07:25:56 -0300

    Models no Laravel 10

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support)
     {
-        return view('admin/supports/index');
+        $supports = $support->all();
+
+        return view('admin/supports/index', compact('supports'));
     }
 }
+https://www.youtube.com/watch?list=PLVSNL1PHDWvQ1N6fqhQ5HQzFtN-xrkjNU&v=7I_uIELCbu4
