ef9ef585d98e9a9176702c7876aa456dbcf01b5b
Lony <loonydarck@hotmail.com>
2023-05-11 17:33:05 -0300

    Atualizar Registro com o Laravel 10

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -40,4 +40,30 @@ class SupportController extends Controller
         $support =  $support->create($data);
         return redirect()->route('supports.index');
     }
+
+    public function edit(Support $support, string | int $id)
+    {
+        if(!$support = $support->where('id', $id)->first()) {
+            return back();
+        }
+
+        return view('admin/supports.edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string $id)
+    {
+
+         
+        if(!$support = Support::find($id)) {
+            return back();
+        }
+
+        $support->update($request->only([
+            'subject', 'body'
+        ]));
+
+        return redirect()->route('supports.index');
+    }
+
+
 }
