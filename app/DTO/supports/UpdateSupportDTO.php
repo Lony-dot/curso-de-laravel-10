bdf2b700b5728769c08d7b01a0595c7aaeae0bbc
Lony <loonydarck@hotmail.com>
2023-05-19 16:40:13 -0300

    Usando ENUMs no Laravel 10

--- app/DTO/supports/UpdateSupportDTO.php
+++ app/DTO/supports/UpdateSupportDTO.php
@@ -10,7 +10,7 @@ class UpdateSupportDTO
     public function __construct(
         public string $id,
         public string $subject,
-        public string $status,
+        public SupportStatus $status,
         public string $body,
     ) {}
 
@@ -19,7 +19,7 @@ class UpdateSupportDTO
         return new self(
             $request->id,
             $request->subject,
-            'a',
+            SupportStatus::A,
             $request->body
         );
     }
