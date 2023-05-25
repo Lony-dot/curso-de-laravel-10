2320984b76a6a454aec3cf0c4d298bcb521ba7cb
Lony <loonydarck@hotmail.com>
2023-05-24 22:23:28 -0300

    Trabalhar com Padrão Adapter no Laravel 10

---
+++ app/Adapters/ApiAdapter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Adapters;
+
+use App\Repositories\PaginationInterface;
+use App\Http\Resources\DefaultResource;
+
+class ApiAdapter
+{
+    public static function toJson(
+        PaginationInterface $data
+    ) {
+        return DefaultResource:: collection($data->items())
+        ->additional([
+            'meta' => [
+                'total' => $data->total(),
+                'is_first_page' => $data->isFirstPage(),
+                'is_last_page' => $data->isLastPage(),
+                'current_page' => $data->currentPage(),
+                'next_page' => $data->getNumberNextPage(),
+                'previous_page' => $data->getNumberPreviousPage(),
+            ]
+        ]);
+    }
+
+}
