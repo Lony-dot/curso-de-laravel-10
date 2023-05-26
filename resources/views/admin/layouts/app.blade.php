4690f5ae9e5817a335314394e5173a73813d184e
Lony <loonydarck@hotmail.com>
2023-05-26 08:41:50 -0300

    refactor: use template and create header

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -9,15 +9,24 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <head>
-        @yield('header')
-    </head>
-    <div class="content">
-        @yield('content')
+    <section class="container px-4 mx-auto">
+    <h3 class="text-2xl text-gray-700 font-bold mb-6 ml-3">
+			Últimas Respostas
+		</h3>
 
-    </div>
-    <footer>
-        #default footer
-    </footer>
+		<ol >
+		  <li class="border-l-2 border-purple-600">
+		    <div class="md:flex flex-start">
+		      <div class="bg-purple-600 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5">
+                @yield('header')
+		    </div>
+        <div class="content">
+            @yield('content')
+
+        </div>
+        <footer>
+            #default footer
+        </footer>
+    </section>
 </body>
 </html>
